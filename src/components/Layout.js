71bccd32eee17f626d871ca8f3088cdbb9043887
Harrison Peace <harrison@apwines.com>
2021-08-29 15:39:42 +1000

    added javascript code formatting and comments

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Layout = (props) => {
-    return (
-      <main>
-        {props.children}
-      </main>
-    );
+  return <main>{props.children}</main>;
 };
 
 export default Layout;
